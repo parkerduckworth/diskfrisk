3c643d9ad7c56e15617045fee4f4ef2d958b1c2b
parkerduckworth <parkerduckworth@gmail.com>
2018-05-27 14:15:10 -0500

    search specific dirs for files, print res

--- task2.c
+++ task2.c
@@ -1,27 +1,38 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 
-void frisk(char* dir)
+int frisk(char* dir, char* file)
 {
     DIR *dirptr = opendir(dir);
     struct dirent *entry;
+    int found = 0;
     
     if (dirptr != NULL) {
-        while (entry = readdir(dirptr))
-            puts(entry->d_name);
-        (void) closedir(dirptr);
+        while ((entry = readdir(dirptr)) != NULL) {
+            if (strcmp(file, entry->d_name) == 0) {
+                puts(entry->d_name);
+                found++;
+            }
+            else
+                continue;
+        }
+        closedir(dirptr);
     }
     else
         perror("Dir Error");
+
+    return (found > 0) ? found : -1;
 
 }
 
 int main()
 {
-    void frisk(char*);
+    int res, frisk(char*, char*);
 
-    frisk("./");
-
-    return 0;
+    res = frisk("./", "filename.png");
+    if (res < 0)
+        printf("File not found...\n");
+    return res;
 }
