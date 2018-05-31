8d6b95a39dd0b58d452fff8bd926971a1ab5571c
parkerduckworth <parkerduckworth@gmail.com>
2018-05-30 20:05:58 -0500

    changed default search dirs

--- diskfrisk.c
+++ diskfrisk.c
@@ -1,3 +1,23 @@
+/* 
+
+Here is a short explanation of how to execute commands, with examples:
+
+If no flags present, your machine's entire file tree is searched.
+~ user$ ./frisk <filename>
+
+To search the 'home' dir (user files) only, input the '-h' flag.
+~ user$ ./frisk -h <filename>
+
+If you want to search only system files, input '-s'.
+~ user$ ./frisk -s <filename>
+
+If both '-h' and '-s' are input, the result is the same as if no 
+flags are input. They can also be input as '-sh' or '-hs'. Order
+doesn't matter here.
+~ user$ ./frisk -h -s <filename>
+
+*/
+
 #include <ctype.h>
 #include <dirent.h>
 #include <limits.h>
@@ -48,7 +68,7 @@ int main(int argc, char *argv[])
 /* Collect user input, parse optional flags */
 char *input(int argc, char *argv[])
 {
-    char c, x;
+    char c, x = 0;
 
     while (--argc > 0 && (*++argv)[0] == '-')
         while ((c = *++argv[0]))
@@ -60,16 +80,18 @@ char *input(int argc, char *argv[])
                     home = 1;
                     break;
                 default:
-                    x = c;  // Save illegal flag to pass to user.
+
+                    // Save illegal flag to pass in error message
+                    x = c;
                     badflag = 1;
                     break;
             }
     if (argc != 1)
         no_fn = 1;
-    else if (home && !sys)
+    if (home && !sys)
         dname = HOME;
 
-    // Update user on current state of process.
+    // Update user on current state of process
     display_state(x, *argv);
 
     return (no_fn + badflag > 0) ? NULL : *argv;
@@ -87,10 +109,10 @@ void display_state(char c, char* fname)
     // Legal input submitted
     else {
         printf("\n\nDISKFRISK -- VERSION 0.0.0\n\n\n");
-        if (home) 
-            printf("Home directory is being frisked...\n");
-        if ((!home && !sys) || sys)
+        if (sys || (!home && !sys))
             printf("System directory is being frisked...\n");
+        if (home || (!home && !sys))
+            printf("Home directory is being frisked...\n");
         printf("Searching for: %s\n\n", fname);
     }
 }
@@ -118,28 +140,30 @@ void traverse(char* fname, char* dname)
     struct dirent *entry;
     struct stat fst;
     char path[PATH_MAX];
-    size_t p_len = strlen(dname);  // Parent dir name length
+    size_t p_len = strlen(dname);  // Current absolute path length
 
     strcpy(path, dname);
+    
+    // Append to existing path string before adding child dir/file
     path[p_len++] = '/';
 
-    // Not ideal, because no error catching for opendir().
+    // Not ideal, because no error catching for opendir()
     if (!(dir = opendir(dname)))
         return;
 
     while ((entry = readdir(dir))) {
 
-        // These directories are not traversed,
-        // they're either hidden or require permission to open.
+        // These directories are not traversed, they're either hidden 
+        // or require permission to open
         if (!(strcmp(entry->d_name, ".")) || !(strcmp(entry->d_name, "..")) ||
             entry->d_name[0] == '.' || !(strcmp(entry->d_name, "Guest")))
                 continue;
 
-        // Home directory skips unless explicitly flagged
-        if (!home && !(strcmp(entry->d_name, "Users")))
+        // If only "-s" is flagged
+        if (!home && sys && !(strcmp(entry->d_name, "Users")))
                 continue;
 
-        // Record absolute path and populate fst
+        // Record absolute path and initialize fst
         strncpy(path + p_len, entry->d_name, PATH_MAX - p_len);
         lstat(path, &fst);
 
