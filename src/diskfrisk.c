48fde59a4f42df84433d61550934ea0ca58aeb20
parkerduckworth <parkerduckworth@gmail.com>
2018-06-05 23:41:55 -0500

    update usage comments and TODO

--- src/diskfrisk.c
+++ src/diskfrisk.c
@@ -1,7 +1,8 @@
 /* TODO
 
+-> integrate grep search from testfile
 -> Combine input flags into struct
--> Maybe a GREP type search for not exact matches
+-> Eliminate case sensitivity
 
 */
 
@@ -14,24 +15,33 @@
 Here is a short explanation of how to execute commands, with examples:
 
 If no flags present, your machine's entire file tree is searched.
-~ user$ ./frisk <filename>
+Ex: ~ user$ ./frisk <filename>
 
 To search the 'home' dir (user files) only, input the '-h' flag.
-~ user$ ./frisk -h <filename>
+Ex: ~ user$ ./frisk -h <filename>
 
 If you want to search only system files, input '-s'.
-~ user$ ./frisk -s <filename>
+Ex: ~ user$ ./frisk -s <filename>
 
 If both '-h' and '-s' are input, the result is the same as if no 
 flags are input. They can also be input as '-sh' or '-hs'. Order
 doesn't matter here.
-~ user$ ./frisk -h -s <filename>
+Ex: ~ user$ ./frisk -h -s <filename>
 
 Directories can be searched, and set to open upon discovery as well.
 
 An '-o' flag is available to automatically open up the first matched 
 result of search.  The search will continue, and all matches will be
 listed.
+
+A '-p' flag is another option that sets all file permission errors to
+display when thrown.
+
+If you wish to search by pattern, rather than by exact match, preface
+your <filename> with 'grep:', and all files containing the matching
+pattern will be output. This can be used in conjuction with any (or all)
+of the optional flags.
+Ex: ~ user$ frisk grep:<filename>
 
 */
 
