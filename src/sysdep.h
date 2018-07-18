194d429e194fc48ddbea24e373a8a2343fd9723f
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2018-07-18 19:45:52 +0100

    Making sysdep.h OS aware.

--- src/sysdep.h
+++ src/sysdep.h
@@ -2,14 +2,25 @@
 #ifndef SYSDEP_H
 #define SYSDEP_H
 
-/* OSX nomenclature */
-#define HNAME     "Users"   // Home directory name
-#define HOME      "/Users"  // Home directory path
-#define ROOT      "/"       // Root directory path
-
 /* Bash syntax */
 #define RDFROMS   "-c"      // Read-from-string option
 #define SHOPEN    "open "   // Open command
 #define SHELL     "sh"      // Initiate new environment context
 
+/* OSX nomenclature */
+#ifdef __APPLE__
+#define HNAME     "Users"   // Home directory name
+#define HOME      "/Users"  // Home directory path
+#define ROOT      "/"       // Root directory path
 #endif
+
+
+//Defining for compilation on Linux
+#ifdef __linux__
+
+#define HNAME     "home"   // Home directory name
+#define HOME      "/home"  // Home directory path
+#define ROOT      "/"      // Root directory path
+#endif
+
+#endif 
